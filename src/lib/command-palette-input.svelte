47bd98dc54afa08f8866509d3b263259ec052474
Matthew Davis <matthew@matthewdavis.io>
2024-10-14 09:39:24 -0500

    bump

--- src/lib/command-palette-input.svelte
+++ src/lib/command-palette-input.svelte
@@ -3,10 +3,11 @@
 
   type Props = {
     onsearch: (value: string) => void;
+    placeholder?: string;
   };
 
   let inputRef: HTMLInputElement;
-  let { onsearch }: Props = $props();
+  let { onsearch, placeholder = 'Search...' }: Props = $props();
 
   const onChange = (e: Event) => {
     const target = e.target as HTMLInputElement;
@@ -19,8 +20,8 @@
 </script>
 
 <div class="relative flex items-center">
-  <svg class="pointer-events-none absolute left-4 top-3.5 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+  <svg class="pointer-events-none m-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
     <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
   </svg>
-  <input bind:this={inputRef} onkeyup={onChange} type="text" class="h-12 flex-1 bg-inherit pl-11 pr-4 text-green-500 outline-none sm:text-sm" placeholder="Search..." />
+  <input bind:this={inputRef} onkeyup={onChange} type="text" class="h-12 flex-1 bg-inherit pr-4 text-green-500 outline-none sm:text-sm" {placeholder} />
 </div>
